9bf40ae4dc0b8e37d0af47212dce7eba706c0255
uppinote <upnote2020@gmail.com>
2026-03-07 06:24:03 +0000

    clavis 0.3.0

--- Formula/clavis.rb
+++ Formula/clavis.rb
@@ -1,22 +1,22 @@
 class Clavis < Formula
   desc "Claude Code system administration tool — TUI & Web"
   homepage "https://github.com/uppinote20/clavis"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/uppinote20/clavis/releases/download/v0.2.1/clavis-aarch64-apple-darwin.tar.gz"
-      sha256 "cf74747b24ad14b860cc98ccfc0dc66a53d94619989a5c342dc9b279e59cfc3f"
+      url "https://github.com/uppinote20/clavis/releases/download/v0.3.0/clavis-aarch64-apple-darwin.tar.gz"
+      sha256 "e7fbaa4aac6e466b821ae91b4a1c130542b25faa9cba682c9badc129a527a4e5"
     else
-      url "https://github.com/uppinote20/clavis/releases/download/v0.2.1/clavis-x86_64-apple-darwin.tar.gz"
-      sha256 "0a943b1e9c1e0e253eea84134c080dc7c57b30d1a65080528a3db005c61142d6"
+      url "https://github.com/uppinote20/clavis/releases/download/v0.3.0/clavis-x86_64-apple-darwin.tar.gz"
+      sha256 "1083877a7b87c12d958af483a1fa4ce2324725614af6e380c324e6c746b13d63"
     end
   end
 
   on_linux do
-    url "https://github.com/uppinote20/clavis/releases/download/v0.2.1/clavis-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "b95b63bba3e2a5c80462c5e80bc9864f1c112e1e0df175c5ba248b01ed32a97a"
+    url "https://github.com/uppinote20/clavis/releases/download/v0.3.0/clavis-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "56462653a13615968c4cef2b647402a0b8673a7055b83e6cf1bc97757f7fa8a0"
   end
 
   def install
